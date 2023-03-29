bdd558cba8d2e555f008e47167c909384b4010e9
zakaria0001 <zakarianabil68@gmail.com>
2023-03-29 12:01:58 +0000

    Project Uploaded

---
+++ src/test/java/com/gestion/g61/GestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.g61;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
